6d4fa044c48c246523baa557c594bf0d97f0cdca
judah-1076823 <143966786+judah-1076823@users.noreply.github.com>
2023-09-29 13:39:31 +0200

    Add files via upload

---
+++ les 1/les_1.py
@@ -0,0 +1,60 @@
+#optellen
+print(2 + 3)
+
+#aftrekken
+print(2 - 3)
+
+#vermenigvuldigen
+print(2 * 3)
+
+#delen
+print(2 / 3)
+
+#vloerdeling?!
+print(2 // 3)
+
+#exponent
+print(2 ** 3)
+
+#strings
+name = "John Doe"
+print(name)
+
+street = 'Neverland Lane'
+print(street)
+
+items = 5
+print(items)
+
+price = 19.95 
+print(price)
+
+students = 28
+teachers = 3
+seats = students + teachers
+print(seats)
+
+correct = students > teachers
+print(correct)
+
+incorrect = students > seats
+print(seats)
+
+line_character = '_'
+line = line_character * 20
+print(line)
+
+city = 'Rotterdam'
+print(city)
+type(city)
+
+residents = 623652
+print(residents)
+type(residents)
+
+area = 324.1
+print(area)
+type(area)
+
+# Formatting string, variabele in string weergeven
+print (f'The city of {city} has {residents} residents on an area of {area} square kilometers')
